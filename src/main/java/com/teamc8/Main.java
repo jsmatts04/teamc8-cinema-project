f7e729aeb4a135e5da97ded590a6c4c33b1db1cd
PhoGaNgon <Kevin152002@gmail.com>
2023-09-26 23:38:26 -0400

    Initial commit

---
+++ src/main/java/com/teamc8/Main.java
@@ -0,0 +1,36 @@
+package com.teamc8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public GreetResponse greet() {
+        GreetResponse response = new GreetResponse(
+                "Hello",
+                List.of("Java", "Lua", "Javascript"),
+                new Person("Kevin", 21, 12000)
+        );
+
+        return response;
+    }
+
+    record Person(String name, int age, double savings) {}
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+    ) {}
+
+}
