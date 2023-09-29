340db87039ee4a5d641f0ceb73c349d384a7eb95
Khang <alexanderdkh@gmail.com>
2023-09-29 00:53:41 -0400

    Added comments for api

--- frontend/src/api/MovieApi.js
+++ frontend/src/api/MovieApi.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/api/movie';
 
-export const fetchMovieCovers = () => axios.get(`${API_URL}/cover`);
+// Returns data for a specific movie specified by id 
 export const fetchMovieById = (id) => axios.get(`${API_URL}/${id}`);
+// Returns a list of all currently showing movies (their title and picture only)
 export const fetchMovieCoversCurrent = () => axios.get(`${API_URL}/cover?status=CURRENT`);
+// Returns a list of all upcoming movies (title and picture only)
 export const fetchMovieCoversUpcoming = () => axios.get(`${API_URL}/cover?status=UPCOMING`);
 
-// export const fetchMovieCoversUpcoming1 = () => axios.get(`${API_URL}/cover`, {params:{status: "UPCOMING"}});
-// export const fetchMovieCoversUpcoming1 = () => axios.get(`${API_URL}/cover`, {params:{status: "CURRENT"}});
+// export const fetchMovieCoversUpcoming = () => axios.get(`${API_URL}/cover`, {params:{status: "UPCOMING"}});
+// export const fetchMovieCoversCurrent = () => axios.get(`${API_URL}/cover`, {params:{status: "CURRENT"}});
