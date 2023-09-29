eff805412b6c2f7fcbab7371f105344bd24c8857
Khang <alexanderdkh@gmail.com>
2023-09-29 00:30:55 -0400

    Co-authored-by: Kevin Nguyen <PhoGaNgon@users.noreply.github.com>

--- frontend/src/api/MovieApi.js
+++ frontend/src/api/MovieApi.js
@@ -4,3 +4,8 @@ const API_URL = 'http://localhost:8080/api/movie';
 
 export const fetchMovieCovers = () => axios.get(`${API_URL}/cover`);
 export const fetchMovieById = (id) => axios.get(`${API_URL}/${id}`);
+export const fetchMovieCoversCurrent = () => axios.get(`${API_URL}/cover?status=CURRENT`);
+export const fetchMovieCoversUpcoming = () => axios.get(`${API_URL}/cover?status=UPCOMING`);
+
+// export const fetchMovieCoversUpcoming1 = () => axios.get(`${API_URL}/cover`, {params:{status: "UPCOMING"}});
+// export const fetchMovieCoversUpcoming1 = () => axios.get(`${API_URL}/cover`, {params:{status: "CURRENT"}});
