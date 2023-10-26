bf4a02ae4e090becda00fe0e8daa62b0941cad9a
PhoGaNgon <Kevin152002@gmail.com>
2023-10-25 21:05:18 -0400

    updated movie cover api endpoint

--- frontend/src/api/MovieApi.js
+++ frontend/src/api/MovieApi.js
@@ -11,8 +11,8 @@ export const fetchMovieById = (id) => api.get(`/${id}`);
 
 // Returns a list of movies by status (CURRENT, UPCOMING)
 // Each movie object contains { id, title, trailerPicture, releaseDate }
-export const fetchMovieCoversCurrent = () => api.get('/cover?status=CURRENT');
-export const fetchMovieCoversUpcoming = () => api.get('/cover?status=UPCOMING');
+export const fetchMovieCoversCurrent = () => api.get('/cover?status=1');
+export const fetchMovieCoversUpcoming = () => api.get('/cover?status=2');
 
 // Post the provided movie into the database
 // Make sure to convert movie to object with toJSON()
