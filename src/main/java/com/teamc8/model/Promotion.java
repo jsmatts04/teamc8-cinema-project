c8ceca8d1145091121d1002312d5763f32a13f87
Saathvik Rukkannagari <Sr27700.uga.edu>
2023-10-28 13:15:23 -0400

    Promotion File

--- src/main/java/com/teamc8/model/Promotion.java
+++ src/main/java/com/teamc8/model/Promotion.java
@@ -1,4 +1,76 @@
 package com.teamc8.model;
 
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "promotion")
 public class Promotion {
+
+    public Promotion(){
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "id", referencedColumnName = "promotion_id")
+    private Booking booking;
+
+    @Column(name = "discount_amount")
+    private int discountAmount;
+
+    @Column(name = "start_date")
+    private Date startDate;
+
+    @Column(name = "end_date")
+    private Date endDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(int amount) {
+        this.discountAmount = amount;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date date) {
+        this.startDate = date;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date date) {
+        this.endDate = date;
+    }
+
+
+
+
 }
+
+
