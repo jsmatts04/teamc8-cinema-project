d769f3b40658cbce1396b2d040c47443922bd7d3
PhoGaNgon <Kevin152002@gmail.com>
2023-10-30 00:58:48 -0400

    removed sharedService

--- frontend/src/api/AxiosConfig.js
+++ frontend/src/api/AxiosConfig.js
@@ -1,10 +1,26 @@
 import axios from 'axios';
-import { getToken, jwtToken } from './SharedService';
+
+//store in localStorage
+export const storeJwtToken = (token) => {
+    localStorage.setItem('jwtToken', token);
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
+
+// Retrieve jwt token from localStorage
+export const getJwtToken = () => {
+    let res = localStorage.getItem('jwtToken');
+    return res === null ? '' : res;
+}
+
+// Removes token from localStorage. Mainly used when logging out
+export const removeToken = () => {
+    localStorage.removeItem('jwtToken');
+}
 
 const api = axios.create({
     baseURL: 'http://localhost:8080/api',
     headers: {
-        'Authorization': `Bearer ${getToken()}`,
+        'Authorization': `Bearer ${getJwtToken()}`,
         'Content-Type': 'application/json'
     }
 });
