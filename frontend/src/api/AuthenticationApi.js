b3e8fcdce6d27089e3901d92cb469f958ddb265f
PhoGaNgon <Kevin152002@gmail.com>
2023-10-30 05:59:04 -0400

    moved another method

--- frontend/src/api/AuthenticationApi.js
+++ frontend/src/api/AuthenticationApi.js
@@ -16,5 +16,9 @@ export const authenticateUser = (req) => axios.post(`${BASE_URL}/auth/authentica
 // Returns a message if email was sent.
 export const resendConfirmationEmail = (email) => axios.get(`${BASE_URL}/auth/resendConfirmation?email=${email}`);
 
+// Reset password without verifying old password
+// Requires token from search params and newPassword from text field
+export const resetPassword = (token, newPassword) => axios.put(`${BASE_URL}/user/resetPassword?${token}`, {'newPassword': newPassword});
+
 // Sends the email to reset password. Takes in the user email
 export const sendResetPasswordEmail = (email) => axios.get(`${BASE_URL}/user/forgotPassword?email=${email}`);
--- frontend/src/api/UserApi.js
+++ frontend/src/api/UserApi.js
@@ -8,10 +8,6 @@ export const editUserProfile = (editUserRequest) => api.put('/user/editProfile',
 // passwordRequest = { email, oldPassword, newPassword } JSON
 export const changePassword = (passwordRequest) => api.put('/user/editProfile/changePassword', passwordRequest);
 
-// Reset password without verifying old password
-// Requires token from search params and newPassword from text field
-export const resetPassword = (token, newPassword) => api.put(`/user/resetPassword?${token}`, {newPassword: newPassword});
-
 // Returns all user info: user, address, and [payment cards]
 // ALL_USER_INFO in reference.
 export const getAllUserInfo = () => api.get(`/user/getAllInfo`);
