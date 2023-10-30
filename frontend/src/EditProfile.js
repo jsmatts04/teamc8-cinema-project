f0a06d2921e9cb75c9ae3e06d6761bec19590cc9
JJ <jw27180@uga.edu>
2023-10-30 06:54:27 -0400

    fixed editprofile

--- frontend/src/EditProfile.js
+++ frontend/src/EditProfile.js
@@ -13,6 +13,8 @@ import { deletePaymentCard } from './api/PaymentCardApi';
 
 function EditProfile() {
   const [hasAddress, setHasAddress] = useState(false);
+  const [isFetched, setIsFetched] = useState(false);
+
   // State for form fields
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
@@ -35,34 +37,36 @@ function EditProfile() {
   const [showPasswordToast, setShowPasswordToast] = useState(false);
 
   useEffect(() => {
-    getAllUserInfo().then(
-      (response) => {
+    async function getAllUsers() {
+      try {
+        const response = await getAllUserInfo();
+
         let data = response.data
-        console.log(data)
-        setFirstName(data.user.firstName)
-        setLastName(data.user.lastName)
-        setPhoneNumber(data.user.phoneNumber)
-        setEmailAddress(data.user.email)
-        if (data.address === null) {
-          setHasAddress(false)
-        } else if (data.address.street !== '' && data.address.city !== '' && data.address.state !== '' && data.address.postalCode !== '') {
-          setHasAddress(true);
-          setAddressId(data.address.id)
-          setStreet(data.address.street)
-          setCity(data.address.city)
-          setState(data.address.state)
-          setPostalCode(data.address.postalCode)
-        }
-        if (data.paymentCards.length !== 0) {
-          setPaymentMethods(data.paymentCards);
-        }
-        setUnsubscribePromos(data.user.promotionEligibility)
+          console.log(data)
+          setFirstName(data.user.firstName)
+          setLastName(data.user.lastName)
+          setPhoneNumber(data.user.phoneNumber)
+          setEmailAddress(data.user.email)
+          if (data.address === null) {
+            setHasAddress(false)
+          } else if (data.address.street !== '' && data.address.city !== '' && data.address.state !== '' && data.address.postalCode !== '') {
+            setHasAddress(true);
+            setAddressId(data.address.id)
+            setStreet(data.address.street)
+            setCity(data.address.city)
+            setState(data.address.state)
+            setPostalCode(data.address.postalCode)
+          }
+          if (data.paymentCards.length !== 0) {
+            setPaymentMethods(data.paymentCards);
+          }
+          setUnsubscribePromos(data.user.promotionEligibility)
+          setIsFetched(true)
+      } catch(err){
+        console.log(err)
       }
-    ).catch(
-      (err) => {
-        console.log(err.code)
-      }
-    )
+    }
+    getAllUsers();
   },[getJwtToken(),showPaymentToastDelete,showPaymentToast])
 
   
@@ -173,111 +177,113 @@ function EditProfile() {
     // You can add logic here to save changes to a database or perform other actions.
   };
   
+  
+
+
   return (
       <div>
-        <div style={{ marginTop:'20px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <div style={{ 'overflow-y':'auto', backgroundColor: '#f8f8f8', padding: '20px', borderRadius: '5px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', maxWidth: '400px', width: '100%' }}>
-            <h1>Edit Profile</h1>
-            <form>
-              <div className="mb-3 row">
-              <div className="col" style={{ marginBottom: '5px' }}>
-                <label htmlFor="firstName" style={{ fontSize: '20px' }}>First Name</label>
-                <input type="text" id="firstName" value={firstName} onChange={handleFirstNameChange} style={{ width: '100%' }} />
-              </div>
-              <div className="col" style={{ marginBottom: '5px' }}>
-                <label htmlFor="lastName" style={{ fontSize: '20px' }}>Last Name</label>
-                <input type="text" id="lastName" value={lastName} onChange={handleLastNameChange} style={{ width: '100%' }} />
-              </div>
-              </div>
-              <div style={{ marginBottom: '5px' }}>
-                <label htmlFor="phoneNumber" style={{ fontSize: '20px' }}>Phone Number</label>
-                <input type="text" id="phoneNumber" value={phoneNumber} onChange={handlePhoneNumberChange} style={{ width: '100%' }} />
-              </div>
-              <div style={{ marginBottom: '5px', fontSize: '20px' }}>
-                <label>Email Address:</label>
-                <a style={{ color: 'gray' }}>{email}</a>
-              </div>
-              <div style={{ marginBottom: '5px', fontSize: '20px' }}>
-                <label>Password:</label>
-                <Link to="/ChangePassword" state={{email:email}} style={{ marginLeft:'2px', color: 'blue', textDecoration: 'underline' }}>Change Password</Link>
-              </div>
-            </form>
+        {
+          !isFetched ? (
+            <h1>Loading...</h1>
+          ) : (
+        <><div style={{ marginTop: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <div style={{ overflowY: 'auto', backgroundColor: '#f8f8f8', padding: '20px', borderRadius: '5px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', maxWidth: '400px', width: '100%' }}>
+                <h1>Edit Profile</h1>
+                <form>
+                  <div className="mb-3 row">
+                    <div className="col" style={{ marginBottom: '5px' }}>
+                      <label htmlFor="firstName" style={{ fontSize: '20px' }}>First Name</label>
+                      <input type="text" id="firstName" value={firstName} onChange={handleFirstNameChange} style={{ width: '100%' }} />
+                    </div>
+                    <div className="col" style={{ marginBottom: '5px' }}>
+                      <label htmlFor="lastName" style={{ fontSize: '20px' }}>Last Name</label>
+                      <input type="text" id="lastName" value={lastName} onChange={handleLastNameChange} style={{ width: '100%' }} />
+                    </div>
+                  </div>
+                  <div style={{ marginBottom: '5px' }}>
+                    <label htmlFor="phoneNumber" style={{ fontSize: '20px' }}>Phone Number</label>
+                    <input type="text" id="phoneNumber" value={phoneNumber} onChange={handlePhoneNumberChange} style={{ width: '100%' }} />
+                  </div>
+                  <div style={{ marginBottom: '5px', fontSize: '20px' }}>
+                    <label>Email Address:</label>
+                    <a style={{ color: 'gray' }}>{email}</a>
+                  </div>
+                  <div style={{ marginBottom: '5px', fontSize: '20px' }}>
+                    <label>Password:</label>
+                    <Link to="/ChangePassword" state={{ email: email }} style={{ marginLeft: '2px', color: 'blue', textDecoration: 'underline' }}>Change Password</Link>
+                  </div>
+                </form>
 
-            <h2>Address</h2>
-            {showAddress(hasAddress)}
-            <h2>Payment Methods ({paymentMethods.length}/3)</h2>
-            {printPaymentMethods()}
-            {(paymentMethods.length < 3) && <Link to="/AddPayment" state={{email: email, addressId: addressId}} style={{ backgroundColor: '#007bff', color: '#fff', border: 'none', padding: '5px 10px', borderRadius: '4px', cursor: 'pointer', textDecoration: 'none' }}>+ Add new payment method</Link>}
+                <h2>Address</h2>
+                {showAddress(hasAddress)}
+                <h2>Payment Methods ({paymentMethods.length}/3)</h2>
+                {printPaymentMethods()}
+                {(paymentMethods.length < 3) && <Link to="/AddPayment" state={{ email: email, addressId: addressId }} style={{ backgroundColor: '#007bff', color: '#fff', border: 'none', padding: '5px 10px', borderRadius: '4px', cursor: 'pointer', textDecoration: 'none' }}>+ Add new payment method</Link>}
 
-            {/* Checkbox for subscribing/unsubscribing from promos */}
-            <div style={{ marginTop: '15px' }}>
-              <label style={{ fontSize: '20px' }}>Promos Subscription</label>
-              <div>
-                <label>
-                  <input type="radio" name="promoSubscription" value="subscribe" checked={unsubscribePromos} onChange={handleUnsubscribePromosChange} /> Subscribe
-                </label>
-                <label>
-                  <input type="radio" name="promoSubscription" value="unsubscribe" checked={!unsubscribePromos} onChange={handleUnsubscribePromosChange} /> Unsubscribe
-                </label>
+                {/* Checkbox for subscribing/unsubscribing from promos */}
+                <div style={{ marginTop: '15px' }}>
+                  <label style={{ fontSize: '20px' }}>Promos Subscription</label>
+                  <div>
+                    <label>
+                      <input type="radio" name="promoSubscription" value="subscribe" checked={unsubscribePromos} onChange={handleUnsubscribePromosChange} /> Subscribe
+                    </label>
+                    <label>
+                      <input type="radio" name="promoSubscription" value="unsubscribe" checked={!unsubscribePromos} onChange={handleUnsubscribePromosChange} /> Unsubscribe
+                    </label>
+                  </div>
+                </div>
+
+                {/* Save Changes button */}
+                <button onClick={handleSaveChanges} style={{ backgroundColor: '#007bff', color: '#fff', border: 'none', padding: '5px 10px', borderRadius: '4px', cursor: 'pointer', marginTop: '10px' }}>Save Changes</button>
+
               </div>
-            </div>
-
-            {/* Save Changes button */}
-            <button onClick={handleSaveChanges} style={{ backgroundColor: '#007bff', color: '#fff', border: 'none', padding: '5px 10px', borderRadius: '4px', cursor: 'pointer', marginTop: '10px'}}>Save Changes</button>
-
-          </div>
-        </div>
-
-        <ToastContainer position="top-center" style={{ zIndex: 1 }}>
-          <Toast show={changesSaved} bg="success" onClose={() => setChangesSaved(false)} animation={true} delay={4000} autohide>
-            <Toast.Header closeButton={true} style={{ background: '#00000010' }}>
-              <strong className="me-auto">Changes Saved</strong>
-            </Toast.Header>
-            <Toast.Body>Your changes have been saved.</Toast.Body>
-          </Toast>
-        </ToastContainer>
-        <div aria-live="polite" aria-atomic="true" className="position-block">
-          <ToastContainer className="p-3" position="top-center" style={{ zIndex: 1 }}>
-            <Toast show={showAddressToast} bg="success" onClose={() => setShowAddressToast(false)} animation={true} delay={4000} autohide>
-                <Toast.Header closeButton={true} style={{ background: '#00000010' }}>
-                    <strong className="me-auto">Address Updated</strong>
-                </Toast.Header>
-                <Toast.Body>Your address has been successfully updated.</Toast.Body>
-            </Toast>
-          </ToastContainer>
-        </div>
-        <div aria-live="polite" aria-atomic="true" className="position-block">
-          <ToastContainer className="p-3" position="top-center" style={{ zIndex: 1 }}>
-            <Toast show={showAddressToastDelete} bg="warning" onClose={() => setShowAddressToastDelete(false)} animation={true} delay={4000} autohide>
-                <Toast.Header closeButton={true} style={{ background: '#00000010' }}>
-                    <strong className="me-auto">Address Deleted</strong>
-                </Toast.Header>
-                <Toast.Body>Your address has been successfully deleted.</Toast.Body>
-            </Toast>
-          </ToastContainer>
-        </div>
-        <div aria-live="polite" aria-atomic="true" className="position-block">
-          <ToastContainer className="p-3" position="top-center" style={{ zIndex: 1 }}>
-            <Toast show={showAddressToast} bg="success" onClose={() => setShowAddressToast(false)} animation={true} delay={4000} autohide>
-                <Toast.Header closeButton={true} style={{ background: '#00000010' }}>
-                    <strong className="me-auto">Payment Method Updated</strong>
-                </Toast.Header>
-                <Toast.Body>Your Payment Method has been successfully updated.</Toast.Body>
-            </Toast>
-          </ToastContainer>
-        </div>
-        <div aria-live="polite" aria-atomic="true" className="position-block">
+            </div><ToastContainer position="top-center" style={{ zIndex: 1 }}>
+                <Toast show={changesSaved} bg="success" onClose={() => setChangesSaved(false)} animation={true} delay={4000} autohide>
+                  <Toast.Header closeButton={true} style={{ background: '#00000010' }}>
+                    <strong className="me-auto">Changes Saved</strong>
+                  </Toast.Header>
+                  <Toast.Body>Your changes have been saved.</Toast.Body>
+                </Toast>
+              </ToastContainer><div aria-live="polite" aria-atomic="true" className="position-block">
                 <ToastContainer className="p-3" position="top-center" style={{ zIndex: 1 }}>
-                    <Toast show={showPasswordToast} bg="success" onClose={() => setShowPasswordToast(false)} animation={true} delay={4000} autohide>
-                        <Toast.Header closeButton={true} style={{ background: '#00000010' }}>
-                            <strong className="me-auto">Password Changed</strong>
-                        </Toast.Header>
-                        <Toast.Body>Your password has been successfully changed.</Toast.Body>
-                    </Toast>
+                  <Toast show={showAddressToast} bg="success" onClose={() => setShowAddressToast(false)} animation={true} delay={4000} autohide>
+                    <Toast.Header closeButton={true} style={{ background: '#00000010' }}>
+                      <strong className="me-auto">Address Updated</strong>
+                    </Toast.Header>
+                    <Toast.Body>Your address has been successfully updated.</Toast.Body>
+                  </Toast>
                 </ToastContainer>
-            </div>
+              </div><div aria-live="polite" aria-atomic="true" className="position-block">
+                <ToastContainer className="p-3" position="top-center" style={{ zIndex: 1 }}>
+                  <Toast show={showAddressToastDelete} bg="warning" onClose={() => setShowAddressToastDelete(false)} animation={true} delay={4000} autohide>
+                    <Toast.Header closeButton={true} style={{ background: '#00000010' }}>
+                      <strong className="me-auto">Address Deleted</strong>
+                    </Toast.Header>
+                    <Toast.Body>Your address has been successfully deleted.</Toast.Body>
+                  </Toast>
+                </ToastContainer>
+              </div><div aria-live="polite" aria-atomic="true" className="position-block">
+                <ToastContainer className="p-3" position="top-center" style={{ zIndex: 1 }}>
+                  <Toast show={showAddressToast} bg="success" onClose={() => setShowAddressToast(false)} animation={true} delay={4000} autohide>
+                    <Toast.Header closeButton={true} style={{ background: '#00000010' }}>
+                      <strong className="me-auto">Payment Method Updated</strong>
+                    </Toast.Header>
+                    <Toast.Body>Your Payment Method has been successfully updated.</Toast.Body>
+                  </Toast>
+                </ToastContainer>
+              </div><div aria-live="polite" aria-atomic="true" className="position-block">
+                <ToastContainer className="p-3" position="top-center" style={{ zIndex: 1 }}>
+                  <Toast show={showPasswordToast} bg="success" onClose={() => setShowPasswordToast(false)} animation={true} delay={4000} autohide>
+                    <Toast.Header closeButton={true} style={{ background: '#00000010' }}>
+                      <strong className="me-auto">Password Changed</strong>
+                    </Toast.Header>
+                    <Toast.Body>Your password has been successfully changed.</Toast.Body>
+                  </Toast>
+                </ToastContainer>
+              </div></>
+          )
+        }
       </div>
-      
   );
 }
 
