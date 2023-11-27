9489668a179a08f972f5a2ba884bd570886f0df6
PhoGaNgon <Kevin152002@gmail.com>
2023-11-26 22:06:00 -0500

    Updated seat generation to match front end

--- src/main/java/com/teamc8/service/SeatService.java
+++ src/main/java/com/teamc8/service/SeatService.java
@@ -25,20 +25,12 @@ public class SeatService {
 
     public void generateSeats(Showtime showtime) {
         List<Seat> seats = new ArrayList<>();
-        char[] rows = {'A', 'B', 'C'};
+        char[] rows = {'A', 'B', 'C', 'D', 'E'};
         for (char row : rows) {
             for (int i = 1; i < 11; i++) {
                 Seat seat = Seat.builder().seatNum(i).seatRow(row).reserved(false).showtime(showtime).build();
                 seats.add(seat);
             }
-        }
-        for (int i = 3; i < 9; i++) {
-            Seat seat = Seat.builder().seatNum(i).seatRow('D').reserved(false).showtime(showtime).build();
-            seats.add(seat);
-        }
-        for (int i = 4; i < 8; i++) {
-            Seat seat = Seat.builder().seatNum(i).seatRow('E').reserved(false).showtime(showtime).build();
-            seats.add(seat);
         }
 
         seatRepository.saveAll(seats);
