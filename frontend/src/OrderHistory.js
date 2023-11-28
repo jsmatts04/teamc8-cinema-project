647f8a6d5bf0defc5b60e8c84e53851c71ac5819
Joel Mathew <jsmatts04@gmail.com>
2023-11-27 22:31:39 -0500

    OrderHistory endpoints implemented

--- frontend/src/OrderHistory.js
+++ frontend/src/OrderHistory.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import DataTable from 'react-data-table-component';
 import { getBookingsForUser } from './api/BookingApi';
 
+
 const columns = [
   {
     name: 'Date',
