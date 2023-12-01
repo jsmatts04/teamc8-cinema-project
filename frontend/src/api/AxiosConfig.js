8fc84db08c44425e050cc77231958fd3a3acbab1
PhoGaNgon <Kevin152002@gmail.com>
2023-12-01 01:16:55 -0500

    fix expired jwt token error

--- frontend/src/api/AxiosConfig.js
+++ frontend/src/api/AxiosConfig.js
@@ -1,11 +1,5 @@
 import axios from 'axios';
 
-//store in localStorage
-export const storeJwtToken = (token) => {
-    localStorage.setItem('jwtToken', token);
-    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-}
-
 // Retrieve jwt token from localStorage
 export const getJwtToken = () => {
     let res = localStorage.getItem('jwtToken');
@@ -24,5 +18,25 @@ const api = axios.create({
         'Content-Type': 'application/json'
     }
 });
+
+// Intercept responses from api
+api.interceptors.response.use(
+    (res) => res,
+    (err) => {
+        console.log("API INTERCEPTOR ERROR");
+        // console.log(err.response.data.message);
+        if (err.response.data.message.startsWith("JWT expired")) {
+            console.log("JWT EXPIRED!!");
+            removeJwtToken();
+        }
+        return Promise.reject(err);
+    }
+)
+
+//store in localStorage
+export const storeJwtToken = (token) => {
+    localStorage.setItem('jwtToken', token);
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
 
 export default api;
